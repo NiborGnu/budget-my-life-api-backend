e3d834dc5558d8485e4fa9a3ac666ae802622060
Robin Ung <Robin_ung@hotmail.com>
2024-10-31 23:00:14 +0000

    Add category, subcategory create and details premission to superuser

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.permissions import BasePermission, IsAuthenticated
+from rest_framework import generics
+from .models import Category, SubCategory
+from .serializers import CategorySerializer, SubCategorySerializer
 
-# Create your views here.
+# Custom permission class
+class IsSuperUser(BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.is_superuser
+
+class CategoryListCreate(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated, IsSuperUser]
+
+class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated, IsSuperUser]
+
+class SubCategoryListCreate(generics.ListCreateAPIView):
+    queryset = SubCategory.objects.all()
+    serializer_class = SubCategorySerializer
+    permission_classes = [IsAuthenticated, IsSuperUser]
+
+class SubCategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = SubCategory.objects.all()
+    serializer_class = SubCategorySerializer
+    permission_classes = [IsAuthenticated, IsSuperUser]

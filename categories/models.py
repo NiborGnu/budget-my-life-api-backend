deb6432864134a8d2bdd6b940af9b3ecc7342cad
Robin Ung <Robin_ung@hotmail.com>
2024-10-31 22:57:57 +0000

    Add category model

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    CATEGORY_TYPES = [
+        ('income', 'Income'),
+        ('expense', 'Expense')
+    ]
+    name = models.CharField(max_length=255)
+    category_type = models.CharField(max_length=7, choices=CATEGORY_TYPES)
+
+    def __str__(self):
+        return self.name
+    
+
+class SubCategory(models.Model):
+    category = models.ForeignKey(
+        Category,
+        related_name='subcategories',
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return f"{self.category.name} - {self.name}"

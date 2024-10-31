702e33ed0679b90cded036402d02b313f3dbca67
Robin Ung <Robin_ung@hotmail.com>
2024-10-31 22:58:22 +0000

    Add category, subcategory serializer

---
+++ categories/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Category, SubCategory
+
+class SubCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubCategory
+        fields = ['id', 'name']
+
+class CategorySerializer(serializers.ModelSerializer):
+    subcategories = SubCategorySerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Category
+        fields = [
+            'id',
+            'name',
+            'category_type',
+            'subcategories'
+        ]

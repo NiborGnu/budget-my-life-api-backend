a0c37cf213e2110c827851613e43929675045f84
Robin Ung <Robin_ung@hotmail.com>
2024-10-31 21:30:12 +0000

    Add transaction list and details view

--- transactions/views.py
+++ transactions/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Transaction
+from .serializers import TransactionSerializer
 
-# Create your views here.
+class TransactionList(generics.ListCreateAPIView):
+    serializer_class = TransactionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # Allow users to only access their own transactions
+        return Transaction.objects.filter(profile__owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(profile=self.request.user.profile)
+
+class TransactionDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # Allow users to only access their own transactions
+        return Transaction.objects.filter(profile__owner=self.request.user)

cc9073e576c7c8e9f7f19a4b33f33f05d7844d42
Robin Ung <Robin_ung@hotmail.com>
2024-11-01 00:35:24 +0000

    Add user serializers

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Profile
+from django.contrib.auth.models import User
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -20,3 +21,20 @@ class ProfileSerializer(serializers.ModelSerializer):
             'name',
             'is_owner',
         ]
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'username',
+            'email',
+            'password',
+        ]
+        extra_kwargs = {
+            "password": {"write_only": True},
+        }
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user

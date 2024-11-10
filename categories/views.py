a68b8453f71977a346037873751f415cbca5115e
Robin Ung <Robin_ung@hotmail.com>
2024-11-10 20:09:35 +0000

    Change categorylist to include category_type

--- categories/views.py
+++ categories/views.py
@@ -1,29 +1,34 @@
-from rest_framework.permissions import BasePermission, IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, BasePermission
 from rest_framework import generics
 from .models import Category, SubCategory
 from .serializers import CategorySerializer, SubCategorySerializer
 
-# Custom permission class
 class IsSuperUser(BasePermission):
     def has_permission(self, request, view):
         return request.user and request.user.is_superuser
 
 class CategoryListCreate(generics.ListCreateAPIView):
-    queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [IsAuthenticated, IsSuperUser]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = Category.objects.all()
+        category_type = self.request.query_params.get('category_type')
+        if category_type:
+            queryset = queryset.filter(category_type=category_type)
+        return queryset
 
 class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [IsAuthenticated, IsSuperUser]
+    permission_classes = [IsAuthenticated]
 
 class SubCategoryListCreate(generics.ListCreateAPIView):
     queryset = SubCategory.objects.all()
     serializer_class = SubCategorySerializer
-    permission_classes = [IsAuthenticated, IsSuperUser]
+    permission_classes = [IsAuthenticated]
 
 class SubCategoryDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = SubCategory.objects.all()
     serializer_class = SubCategorySerializer
-    permission_classes = [IsAuthenticated, IsSuperUser]
+    permission_classes = [IsAuthenticated]

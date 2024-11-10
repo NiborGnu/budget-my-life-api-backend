991937de6a7c80f3aee1922141e0e2143dbf3e87
Robin Ung <Robin_ung@hotmail.com>
2024-11-10 20:10:46 +0000

    Add Categories to transactions

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
 from .models import Transaction
+from categories.models import Category, SubCategory
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
+
+class SubCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubCategory
+        fields = ['id', 'name']
 
 class TransactionSerializer(serializers.ModelSerializer):
     profile = serializers.ReadOnlyField(source='profile.owner.username')
+    category = CategorySerializer(read_only=True)
+    subcategory = SubCategorySerializer(read_only=True)
+    category_id = serializers.IntegerField(write_only=True)
+    subcategory_id = serializers.IntegerField(write_only=True)
 
     class Meta:
         model = Transaction

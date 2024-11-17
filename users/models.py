6a018b0cf75f263f2dc1f364a8686dac71d72850
Robin Ung <Robin_ung@hotmail.com>
2024-11-17 22:03:33 +0000

    Changed Profile to have first and last name not simply name. Del about section

--- users/models.py
+++ users/models.py
@@ -7,8 +7,8 @@ class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    name = models.CharField(max_length=255, blank=True)
-    content = models.TextField(blank=True)
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
 
     class Meta:
         ordering = ['-created_at']

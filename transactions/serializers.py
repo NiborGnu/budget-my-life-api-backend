7d8c252cc6afbd35439f2570b52f2a6f22fe3c44
Robin Ung <Robin_ung@hotmail.com>
2024-12-12 12:59:55 +0000

    Add budgets to transactions

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -1,23 +1,20 @@
 from rest_framework import serializers
 from .models import Transaction
 from categories.models import Category, SubCategory
+from categories.serializers import CategorySerializer, SubCategorySerializer
+from budgets.models import Budget
 
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ['id', 'name']
-
-class SubCategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SubCategory
-        fields = ['id', 'name']
 
 class TransactionSerializer(serializers.ModelSerializer):
     profile = serializers.ReadOnlyField(source='profile.owner.username')
     category = CategorySerializer(read_only=True)
     subcategory = SubCategorySerializer(read_only=True)
-    category_id = serializers.IntegerField(write_only=True)
-    subcategory_id = serializers.IntegerField(write_only=True)
+    
+    # We only show the budget_id here, but not the full Budget object
+    budget_id = serializers.IntegerField(write_only=True, required=False, allow_null=True)
+    
+    category_id = serializers.IntegerField(write_only=True, required=True)
+    subcategory_id = serializers.IntegerField(write_only=True, required=False, allow_null=True)
 
     class Meta:
         model = Transaction
@@ -28,8 +25,51 @@ class TransactionSerializer(serializers.ModelSerializer):
             'transaction_type',
             'category',
             'subcategory',
+            'budget_id',  # Display only the budget_id, not the full budget
             'category_id',
             'subcategory_id',
             'description',
-            'created_at'
+            'created_at',
         ]
+
+    def create(self, validated_data):
+        category_id = validated_data.pop('category_id', None)
+        subcategory_id = validated_data.pop('subcategory_id', None)
+        budget_id = validated_data.pop('budget_id', None)
+        profile = self.context['request'].user.profile
+
+        if category_id:
+            validated_data['category'] = Category.objects.get(id=category_id)
+        if subcategory_id:
+            validated_data['subcategory'] = SubCategory.objects.get(id=subcategory_id)
+        if budget_id:
+            validated_data['budget'] = Budget.objects.get(id=budget_id)
+
+        validated_data.pop('profile', None)
+        return Transaction.objects.create(profile=profile, **validated_data)
+
+    def update(self, instance, validated_data):
+        category_id = validated_data.pop('category_id', None)
+        subcategory_id = validated_data.pop('subcategory_id', None)
+        budget_id = validated_data.pop('budget_id', None)
+
+        if category_id is None:
+            instance.category = None
+        elif category_id:
+            instance.category = Category.objects.get(id=category_id)
+
+        if subcategory_id is None:
+            instance.subcategory = None
+        elif subcategory_id:
+            instance.subcategory = SubCategory.objects.get(id=subcategory_id)
+
+        if budget_id is None:
+            instance.budget = None
+        elif budget_id:
+            instance.budget = Budget.objects.get(id=budget_id)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance

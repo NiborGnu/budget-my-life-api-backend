17bd7b5d21a7017087c88bbe7c35497156bab25e
Robin Ung <Robin_ung@hotmail.com>
2024-12-12 12:57:54 +0000

    Change admin budget view

--- budgets/admin.py
+++ budgets/admin.py
@@ -3,6 +3,13 @@ from .models import Budget
 
 @admin.register(Budget)
 class BudgetAdmin(admin.ModelAdmin):
-    list_display = ('name', 'amount', 'profile', 'category', 'created_at')
-    search_fields = ('name', 'profile__owner__username', 'category__name')
+    list_display = (
+        'name',
+        'amount',
+        'profile',
+        'category',
+        'created_at',
+        'updated_at'
+    )
+    search_fields = ('name', 'profile__owner__username')
     list_filter = ('category', 'created_at')
